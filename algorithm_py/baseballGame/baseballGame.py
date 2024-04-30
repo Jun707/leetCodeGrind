f1c36ac0b6c67a8814e62deec9c3dfc2fc951acb
Jun707 <juntowork@gmail.com>
2024-04-30 11:06:48 -0600

    Added a better approach for 682

--- algorithm_py/baseballGame/baseballGame.py
+++ algorithm_py/baseballGame/baseballGame.py
@@ -30,3 +30,19 @@ class Solution:
                     nums.append(add1)
                     nums.append(add)
         return sum(nums)
+
+# Better Approach
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack = []
+        
+        for ops in operations:
+            if ops == "+":
+                stack.append(stack[-1]+stack[-2])
+            elif ops == "D":
+                stack.append(stack[-1]*2)
+            elif ops =="C":
+                stack.pop()
+            else:
+                stack.append(int(ops))
+        return sum(stack)
