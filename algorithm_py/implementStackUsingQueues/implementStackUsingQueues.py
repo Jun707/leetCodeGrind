b3acddd98fd48bfd602ee1afb56b1e236c93f4be
Jun707 <juntowork@gmail.com>
2024-05-23 21:21:51 -0600

    updated correct solution

--- algorithm_py/implementStackUsingQueues/implementStackUsingQueues.py
+++ algorithm_py/implementStackUsingQueues/implementStackUsingQueues.py
@@ -1,16 +1,18 @@
 class MyStack:
 
     def __init__(self):
-        self.arr = []
+        self.arr = deque()
         
     def push(self, x: int) -> None:
         self.arr.append(x)
         
     def pop(self) -> int:
-        return self.arr.pop()
+        for i in range(len(self.arr)-1):
+            self.arr.append(self.arr.popleft())
+        return self.arr.popleft()
         
     def top(self) -> int:
         return self.arr[-1]
-        
+
     def empty(self) -> bool:
-        return len(self.arr) == 0
+        return len(self.arr)==0
