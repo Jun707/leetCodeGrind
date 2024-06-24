ac3d5ae80f53b323788dc60b09acdf9e89002dd3
Jun707 <juntowork@gmail.com>
2024-06-23 21:07:53 -0600

    230.kth-smallest-in-bst

---
+++ algorithm_py/230.kth-smallest-in-bst/230.kth-smallest-in-bst.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        res = deque()
+        def inorder(root):
+            if not root:
+                return
+            
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+        
+        inorder(root)
+        while k > 1:
+            res.popleft()
+            k-=1
+        return res.popleft()
